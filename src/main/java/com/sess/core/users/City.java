aef75940a556197b838ef6a02ebc196ebdfdb404
Artur Skorik <admskorik@gmail.com>
2020-12-30 18:40:00 +1000

    added entities

---
+++ src/main/java/com/sess/core/users/City.java
@@ -0,0 +1,169 @@
+package com.sess.core.users;
+
+import javax.persistence.*;
+
+public class City {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cities_id_seq"
+    )
+    @SequenceGenerator(
+            name = "cities_id_seq",
+            sequenceName = "cities_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(name = "address", length = 1024, unique = true, nullable = false)
+    private String address;
+
+    @Column(name = "postal_code", length = 100)
+    private String postalCode;
+
+    @Column(name = "federal_district", length = 100, nullable = false)
+    private String federalDistrict;
+
+    @Column(name = "region_type", length = 100, nullable = false)
+    private String regionType;
+
+    @Column(name = "region", length = 100, nullable = false)
+    private String region;
+
+    @Column(name = "city", length = 100)
+    private String city;
+
+    @Column(name = "kladr_id", length = 100, nullable = false, unique = true)
+    private String kladrId;
+
+    @Column(name = "fias_id", length = 100, nullable = false, unique = true)
+    private String fiasId;
+
+    @Column(name = "fias_level", nullable = false)
+    private Integer fiasLevel;
+
+    @Column(name = "okato", length = 100, nullable = false)
+    private String okato;
+
+    @Column(name = "oktmo", length = 100, nullable = false)
+    private String oktmo;
+
+    @Column(name = "tax_office", length = 100, nullable = false)
+    private String taxOffice;
+
+    @Column(name = "timezone", length = 50, nullable = false)
+    private String timezone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getFederalDistrict() {
+        return federalDistrict;
+    }
+
+    public void setFederalDistrict(String federalDistrict) {
+        this.federalDistrict = federalDistrict;
+    }
+
+    public String getRegionType() {
+        return regionType;
+    }
+
+    public void setRegionType(String regionType) {
+        this.regionType = regionType;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getKladrId() {
+        return kladrId;
+    }
+
+    public void setKladrId(String kladrId) {
+        this.kladrId = kladrId;
+    }
+
+    public String getFiasId() {
+        return fiasId;
+    }
+
+    public void setFiasId(String fiasId) {
+        this.fiasId = fiasId;
+    }
+
+    public Integer getFiasLevel() {
+        return fiasLevel;
+    }
+
+    public void setFiasLevel(Integer fiasLevel) {
+        this.fiasLevel = fiasLevel;
+    }
+
+    public String getOkato() {
+        return okato;
+    }
+
+    public void setOkato(String okato) {
+        this.okato = okato;
+    }
+
+    public String getOktmo() {
+        return oktmo;
+    }
+
+    public void setOktmo(String oktmo) {
+        this.oktmo = oktmo;
+    }
+
+    public String getTaxOffice() {
+        return taxOffice;
+    }
+
+    public void setTaxOffice(String taxOffice) {
+        this.taxOffice = taxOffice;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+}
---
+++ src/main/java/com/sess/core/users/User.java
@@ -0,0 +1,118 @@
+package com.sess.core.users;
+
+import org.hibernate.annotations.Type;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "users_id_seq"
+    )
+    @SequenceGenerator(
+            name = "users_id_seq",
+            sequenceName = "users_id_seq",
+            allocationSize = 1
+    )
+    private Long id;
+
+    @Column(name = "nickname", unique = true, length = 128, nullable = false)
+    @NotBlank(message = "Поле \"Ник\" обязательно для заполнения")
+    @Size(max = 128, message = "Поле \"Ник\" не может превышать 128 символов")
+    private String nickname;
+
+    @Column(name = "email", unique = true, length = 128, nullable = false)
+    @NotBlank(message = "Поле \"email\" обязательно для заполнения")
+    @Email(message = "Некорректный формат ввода поля \"email\"")
+    @Size(max = 128, message = "Поле \"email\" не может превышать 128 символов")
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id", nullable = false)
+    @NotNull(message = "Поле \"Город\" обязательно для заполнения")
+    private City city;
+
+    @Column(name = "sex", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull(message = "Поле \"Пол\" обязательно для заполнения")
+    private Sex sex;
+
+    @Column(name = "birthday")
+    @Convert(converter = Jsr310JpaConverters.LocalDateTimeConverter.class)
+    @NotNull(message = "Поле \"Дата рождения\" обязательно для заполнения")
+    private LocalDateTime birthday;
+
+    @Column(name = "security_key", unique = true, length = 100, nullable = false)
+    @Type(type="uuid-char")
+    private UUID securityKey;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    public UUID getSecurityKey() {
+        return securityKey;
+    }
+
+    public void setSecurityKey(UUID securityKey) {
+        this.securityKey = securityKey;
+    }
+}
