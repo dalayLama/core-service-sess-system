2c626d12c697f1b37bae46668292f0105771312f
Artur Skorik <admskorik@gmail.com>
2021-01-02 18:35:42 +1000

    added scripts for roles

---
+++ src/main/resources/db/migration/V5__Load_table_roles.sql
@@ -0,0 +1,4 @@
+insert into roles (id, name, caption) values (1, 'Admin', 'Администратор');
+insert into roles (id, name, caption) values (2, 'Curator', 'Куратор');
+insert into roles (id, name, caption) values (3, 'Moderator', 'Модератор');
+insert into roles (id, name, caption) values (4, 'Participant', 'Участник');
