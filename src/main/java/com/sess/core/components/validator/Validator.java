d371c0503dc218251023284232bc13ab565ab2b3
Artur Skorik <admskorik@gmail.com>
2021-01-03 11:24:24 +1000

    modified validator

--- src/main/java/com/sess/core/components/validator/Validator.java
+++ src/main/java/com/sess/core/components/validator/Validator.java
@@ -1,9 +1,13 @@
 package com.sess.core.components.validator;
 
+import com.sess.core.exceptions.ValidationException;
+
 import java.util.Set;
 
 public interface Validator {
 
     Set<String> validate(Object object);
+
+    void throwExceptionIfHasErrors(Object object) throws ValidationException;
 
 }
