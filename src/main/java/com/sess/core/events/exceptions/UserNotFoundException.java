a1696c224d26ddb328a87241bfce497c4bb904f3
Artur Skorik <admskorik@gmail.com>
2021-01-03 11:28:24 +1000

    changed UserNotFoundException

--- src/main/java/com/sess/core/events/exceptions/UserNotFoundException.java
+++ src/main/java/com/sess/core/events/exceptions/UserNotFoundException.java
@@ -1,10 +1,11 @@
 package com.sess.core.events.exceptions;
 
-import com.sess.core.exceptions.Error;
+import com.sess.core.exceptions.ErrorBuilder;
+import com.sess.core.exceptions.ErrorMessage;
 import com.sess.core.exceptions.OperationAppException;
 
 public class UserNotFoundException extends OperationAppException {
-    public UserNotFoundException(Error error) {
-        super(error);
+    public UserNotFoundException(ErrorMessage error) {
+        super(ErrorBuilder.singletonMessage(false, error));
     }
 }
