8ed45ee84f68b46d918310309e3e13a814eb509f
Artur Skorik <admskorik@gmail.com>
2021-01-04 16:50:49 +1000

    added city rest api

---
+++ src/main/java/com/sess/core/api/rest/handlers/CityRestApiHandler.java
@@ -0,0 +1,11 @@
+package com.sess.core.api.rest.handlers;
+
+import com.sess.core.dto.DTOCity;
+
+import java.util.List;
+
+public interface CityRestApiHandler {
+
+    List<DTOCity> getAllCities();
+
+}
